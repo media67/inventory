a30a00ef1cef60125c65978078b195ef9096124e
Samuel Bourassa <district67@gmail.com>
2023-03-07 11:43:19 -0500

    Add a function, in the ViewModel,  to retrieve item details in the database based on the id.

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -35,6 +35,10 @@ class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
         return true
     }
 
+    fun retrieveItem(id: Int): LiveData<Item> {
+        return itemDao.getItem(id).asLiveData()
+    }
+
 }
 
 class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
