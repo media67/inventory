05812c9c4985f2f9dd9185852d487a97c32769aa
Samuel Bourassa <district67@gmail.com>
2023-03-07 12:41:43 -0500

    Add the edit Item feature: reuse the Add Item screen by updating the fragment title to Edit Item, add a click listener to the FAB, to navigate to Edit Item screen.

--- app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -73,7 +73,15 @@ class ItemDetailFragment : Fragment() {
             sellItem.isEnabled = viewModel.isStockAvailable(item)
             sellItem.setOnClickListener { viewModel.sellItem(item) }
             deleteItem.setOnClickListener { showConfirmationDialog() }
+            editItem.setOnClickListener { editItem() }
         }
+    }
+
+    private fun editItem() {
+        val action = ItemDetailFragmentDirections.actionItemDetailFragmentToAddItemFragment(
+            getString(R.string.edit_fragment_title),
+            item.id
+        )
     }
 
     /**
